ff12f5d4326c6a785d0cc88a42338486e4635cf8
Octowl <alsaff1987@gmail.com>
2015-08-25 18:03:45 +0300

    Specced out first FT using unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,35 @@
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+import unittest
 
-assert 'Django' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_register(self):
+        # Dr. Bu Sale7 has heard about a cool new KISR website that helps you find projects and fellow reserchers
+        #  He checks out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # He notices the page title and header say 'KISRWho'
+        self.assertIn('KISRWho', self.browser.title)
+        self.fail('Finish the tests!')
+
+        # He sees a register button on the Navbar
+
+        # When he clicks it, it takes him to a registration form asking for his Name, Email, Username and Password
+
+        # He fills it out with his details
+
+        # When he hits enter, the register button goes away and is replaced by his username and a logout button
+
+        # He's also been redirected to his own profile page
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
